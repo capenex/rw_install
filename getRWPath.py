f561f7aa9b350feae99873cf9e46b86caf18f48f
081 <capenex@outlook.com>
2023-05-28 04:51:32 +0800

    界面bug修复，找路径、判断版本功能完成

--- getRWPath.py
+++ getRWPath.py
@@ -1,4 +1,86 @@
 import os
-class RWPath(object):
-    def get_path(path):
-        lst = os.walk(path)
+import configparser as inpar
+import logging
+
+progress = 0
+
+def get_path(disk: str) -> list:
+    if disk[0].upper() not in [chr(i) for i in range(65, 91)]:   
+        print('驱动器错误')
+        return []
+    p = disk[0].upper() + ':'
+    
+    try:
+        dir_lst = os.listdir(p)
+        l = len(dir_lst)
+    except FileNotFoundError as e:
+        print(e)
+        return []
+    all = os.walk(p + '\\')
+    pathLst = []
+    progress_new = 0
+    global progress
+    for tup in all:
+        path, dir, file = tup
+        # r"E:\game\rw\1.15\Rusted Warfare\Rusted Warfare.exe"
+        dirname = path[3:(path+'\\').index('\\', 3)]
+        if dirname and dirname in dir_lst:
+            progress = dir_lst.index(dirname)
+            if progress != progress_new:
+                print(f"{progress}/{l}")
+                progress_new = progress
+        if 'Rusted Warfare.exe' in file:
+            pathLst.append(path)
+    return pathLst
+
+
+def is_1_15(path: str) -> bool:
+    if path[-1] != '/' or path[-1] != '\\':
+        path += '/'
+    outpostpath = path + "assets/units/outpost/outpost.ini"
+    try:
+        conf = inpar.ConfigParser()
+        conf.read(outpostpath)
+        price = conf["core"]["price"]
+        if price == "1500":
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(e)
+        return False
+
+def log_found_paths(pathLst: list, disks: list):
+    logging.basicConfig(filename='info.log', filemode='w', format="%(asctime)s %(name)s:%(levelname)s:%(message)s", datefmt="%d-%M-%Y %H:%M:%S", level=logging.INFO)
+    for path in pathLst:
+        ver = '1.15' if is_1_15(path) else '1.14'
+        logging.info(f'   version:{ver} {path}') 
+    if disks:
+        s = " disk: "
+        for d in disks:
+            s += d[0] + " "
+        logging.info(s + ' read')
+    return pathLst
+
+def read_log(filePath):
+    lst = []
+    lastline_2 = ""
+    lastChoice = ""
+    with open(filePath, 'r') as f:
+        for line in f.readlines():
+            
+            if 'read' in line and 'disk' in line:
+                lastline_2 = line
+                continue
+            if 'lastuse:' in line and 'Rusted Warfare' in line:
+                path = line[line.index('lastuse:')+13:-1]                
+                lastChoice = path
+                continue
+            ver = line[line.index('version:')+8: line.index('version:')+12]
+            path = line[line.index('version:')+13:-1]
+            lst.append((ver, path))
+    return (lst, lastline_2, lastChoice)
+
+def last_use(path):
+    logging.basicConfig(filename='info.log', filemode='a', format="%(asctime)s %(name)s:%(levelname)s:%(message)s", datefmt="%d-%M-%Y %H:%M:%S", level=logging.INFO)
+    logging.info('   lastuse:     ' + path)
--- interface.py
+++ interface.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import threading
 from tkinter import filedialog
+import getRWPath
+import os
 
 FONT_SIZE_LABEL = 15
 FONT_SIZE_BUTTON = 12
@@ -16,17 +18,19 @@ BODY_HEIGHT = 260
 WINDOW_WIDTH = BODY_WIDTH + FONT_SIZE_LABEL * PADDING_LEFT * 2
 WINDOW_HEIGHT = BODY_HEIGHT + FONT_SIZE_LABEL * (PADDING_TOP + PADDING_BOTTON) * 2
 
-
+disk = ['D']
 
 class UI(object):
     def __init__(self):
         self.root = tk.Tk()
         self.root.geometry(f'{WINDOW_WIDTH}x{WINDOW_HEIGHT}')
         self.root.title('rw_installer')
-        self._v = tk.IntVar()
+
+        self._v = tk.IntVar()   # 摇了我把这两个玩意实在不知道放哪
+        self._v3_s = ""
         # 属性
-        self.gamePaths = ['C:/path/to/file']
-        self.selectPath = self.gamePaths[0]
+        self.gamePaths = []
+        
         # 控件
         # 占位
         tk.Label(self.root, text="", width=PADDING_LEFT, height=PADDING_TOP).grid(row=0, column=0)
@@ -38,7 +42,7 @@ class UI(object):
         self.l1.grid(row=1, column=1, columnspan=3)
         
         self.inpTxtV1 = tk.StringVar(value='')
-        self.inp1 = tk.Entry(self.root, textvariable=self.inpTxtV1, font=(FONT_FAMILY, FONT_SIZE_LABEL))   # width=round(BODY_WIDTH/FONT_SIZE_LABEL)
+        self.inp1 = tk.Entry(self.root, textvariable=self.inpTxtV1, font=(FONT_FAMILY, FONT_SIZE_LABEL))
         self.inp1.grid(row=2, column=0, columnspan=3)
 
         self.btn1 = tk.Button(self.root, text="浏览文件", font=(FONT_FAMILY, FONT_SIZE_BUTTON), command=self.browseFiles)
@@ -49,16 +53,27 @@ class UI(object):
         self.l2.grid(row=3, column=1, columnspan=3)
 
         self.inpTxtV2 = tk.StringVar(value='')
-        self.inp2 = tk.Entry(self.root, textvariable=self.inpTxtV2, font=(FONT_FAMILY, FONT_SIZE_LABEL))   # width=round(BODY_WIDTH/FONT_SIZE_LABEL)
+        self.inp2 = tk.Entry(self.root, textvariable=self.inpTxtV2, font=(FONT_FAMILY, FONT_SIZE_LABEL))
         self.inp2.grid(row=4, column=0, columnspan=3)
 
         self.btn2 = tk.Button(self.root, text="浏览路径", font=(FONT_FAMILY, FONT_SIZE_BUTTON), command=self.browsePaths)
         self.btn2.grid(row=4, column=3, columnspan=2)
-
+        
         self.insBtn = tk.Button(self.root, text='安装', font=(FONT_FAMILY, FONT_SIZE_BUTTON), command=self.start_install)
         self.insBtn.place(x=30, y=BODY_HEIGHT-25-2*FONT_SIZE_BUTTON)
         self.cancelBtn = tk.Button(self.root, text='取消', font=(FONT_FAMILY, FONT_SIZE_BUTTON), command=self.stop)
         self.cancelBtn.place(x=BODY_WIDTH-30 - 20 - 2*FONT_SIZE_BUTTON, y=BODY_HEIGHT-25-2*FONT_SIZE_BUTTON)
+
+        self.log_read()
+        if self.gamePaths:    
+            self.selectPath = self.gamePaths[0]
+        else:
+            self.selectPath = ""
+        if self.inpTxtV2.get() != '':
+            if getRWPath.is_1_15(self.inpTxtV2.get()):
+                self.txtV2.set('(1.15)')
+            else:
+                self.txtV2.set('(1.14)')
 
     def mainloop(self):
         self.root.mainloop()
@@ -68,38 +83,80 @@ class UI(object):
     
 
     def browseFiles(self):
-        
         filename = filedialog.askopenfilename(
             initialdir = "/",
             title = "Select a File",
             filetypes = (("all files", "*.*"), ("all files", ""))
         )
-
         self.inpTxtV1.set(filename)
     
     def browsePaths(self):
-        
         self.toplevel = tk.Toplevel(self.root)
         self.toplevel.geometry('400x200')
         self._v.set(0)
-        for i, path in enumerate(self.gamePaths):
-            tk.Radiobutton(self.toplevel, text=path, variable=self._v, value=i, font=(FONT_FAMILY, FONT_SIZE_PATH)).pack(anchor="w")
         
+        self.txtV3 = tk.StringVar(value="当前已扫描了D盘")
+        if self._v3_s:
+            self.txtV3.set(self._v3_s)
+        self.l3 = tk.Label(self.toplevel, textvariable=self.txtV3, font=(FONT_FAMILY, FONT_SIZE_LABEL))
+        self.l3.pack()
+
+        self.l4 = tk.Label(self.toplevel, text='扫描其他盘', font=(FONT_FAMILY, FONT_SIZE_LABEL))
+        self.l4.pack()
+        self.inpTxtV3 = tk.StringVar(value='E')
+        self.inp3 = tk.Entry(self.toplevel, textvariable=self.inpTxtV3, font=(FONT_FAMILY, FONT_SIZE_LABEL))
+        self.inp3.pack()
+        tk.Button(self.toplevel, text='扫描', font=(FONT_FAMILY, FONT_SIZE_BUTTON), command=self.scan_disk).pack()
+
+        self.rdbLst = []
+        for i, path in enumerate(self.gamePaths):
+            if getRWPath.is_1_15(path):
+                ver = '(1.15)'
+            else:
+                ver = '(1.14)'
+            name = ver + ' ' + path
+            self.rdbLst.append(tk.Radiobutton(self.toplevel, text=name, variable=self._v, value=i, font=(FONT_FAMILY, FONT_SIZE_PATH)))
+            self.rdbLst[i].pack(anchor="w")
+
         tk.Button(self.toplevel, text="选好了", command=self.set_path, font=(FONT_FAMILY, FONT_SIZE_BUTTON)).pack()
 
-    def verSelect(self):
-        pass
-
     def start_install(self):
-        threading.Thread(self._start_install).start()
+        threading.Thread(target=self._start_install).start()
 
     def _start_install(self):
+        getRWPath.last_use(self.inpTxtV2.get())
         pass
 
     def set_path(self):
         self.inpTxtV2.set(self.gamePaths[self._v.get()])
+        if getRWPath.is_1_15(self.inpTxtV2.get()):
+            ver = '(1.15)'
+        else:
+            ver = '(1.14)'
+        txtV2Set = ver
+        self.txtV2.set(txtV2Set)
         self.toplevel.destroy()
         
-ui = UI()
+    def log_read(self):
+        if not os.path.exists('info.log'):
+            self.txtV2.set('首次使用，请点击浏览路径')
+            return
+        lst, Lastline, lastUse = getRWPath.read_log('info.log')
+        lastLineList = Lastline.split()
+        diskList = lastLineList[lastLineList.index('disk:')+1: -1]
+        diskStr = ",".join(diskList)
+        self._v3_s = '当前已扫描' + diskStr + '盘'
+        self.inpTxtV2.set(lastUse)
+        self.gamePaths.extend([tup[1] for tup in lst])
 
+    def scan_disk(self):
+        lst = getRWPath.get_path(self.inpTxtV3.get()[0])
+        lst = getRWPath.log_found_paths(lst, [self.inpTxtV3.get()[0]])
+        self.inpTxtV3.set(self.inpTxtV3.get()[0])
+        self.txtV3.set('当前扫描了' + self.inpTxtV3.get()[0] + '盘')
+        self.gamePaths = lst
+        self.toplevel.destroy()
+        self.browsePaths()
+
+ui = UI()
 ui.mainloop()
